0a6c4fa31898f5f8cc511a5c1b691c3e452554cb
luismrr1 <luismrr.fie@gmail.com>
2025-01-29 07:26:33 -0500

    funcion de ingresar nombre y listar html

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+
+
+function agregarAmigo() {
+    let nombreIngresado = document.getElementById("amigo").value;
+    console.log(`${nombreIngresado}`);
+    amigos.push(nombreIngresado);
+    console.log(amigos);
+    listaAmigosHtml();
+}
+
+//console.log(amigos);
+
+function listaAmigosHtml(){
+    let listaHtml = document.getElementById("listaAmigos");
+    listaHtml.innerHTML= "";
+
+    amigos.forEach((item) => {
+        const elementoLista = document.createElement("li");
+        elementoLista.textContent = item;
+        listaHtml.append(elementoLista);
+    });
+
+}
