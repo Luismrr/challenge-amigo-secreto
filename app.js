150f983c1feed7bcec0053f6cecedc0a114f9123
luismrr1 <luismrr.fie@gmail.com>
2025-01-30 05:44:52 -0500

    sortear completo y programam funcionando

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos=[];
-
+let indexAleatorio;
 
 function agregarAmigo() {
     let nombreIngresado = document.getElementById("amigo").value;
     console.log(`${nombreIngresado}`);
-    amigos.push(nombreIngresado);
-    console.log(amigos);
-    listaAmigosHtml();
+
+    // si se ingresó un nombre agregarlo al arreglo amigos
+    if (nombreIngresado!="") {
+        amigos.push(nombreIngresado);
+        document.querySelector('#amigo').value = '';
+   //console.log(amigos);
+    listaAmigosHtml();  // y se llama  a la lista html
+    }
 }
 
-//console.log(amigos);
-
+// Toma la lista amigos, la limpia y llena con elementos del arreglo
 function listaAmigosHtml(){
     let listaHtml = document.getElementById("listaAmigos");
     listaHtml.innerHTML= "";
@@ -19,7 +23,21 @@ function listaAmigosHtml(){
     amigos.forEach((item) => {
         const elementoLista = document.createElement("li");
         elementoLista.textContent = item;
-        listaHtml.append(elementoLista);
+        listaHtml.append(elementoLista); 
     });
-
 }
+
+// genera elemento aleatorio del arreglo amigo y lo muestra como resultado
+function sortearAmigo(){
+    if (amigos.length>0){
+        indexAleatorio = Math.floor(Math.random()*amigos.length);
+        //console.log(indexAleatorio);
+    } else { alert("Debes ingresar nombres");
+    }
+
+    var amigoSorteado = amigos[indexAleatorio];
+    //console.log(amigoSorteado);
+    let valorsorteado = document.getElementById("resultado");
+    valorsorteado.innerHTML="";
+    valorsorteado.textContent = amigoSorteado;
+    }
